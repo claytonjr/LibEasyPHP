d45c7c4674c626cd575747cd83d1d031eb5fec8b
claytonjr <james.r.clayton@gmail.com>
2013-06-19 10:20:12 -0300

    Create LibNetwork.php

---
+++ LibNetwork.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * LibNetwork - A library of functions for working with networks. 
+ * @author James Clayton <james.r.clayton@gmail.com>
+ * @version 0.0.1
+ * @copyright (c) 2013, James Clayton
+ * @package LibNetwork
+ * @license http://opensource.org/licenses/ISC ISC License (ISC)
+ */
+
+?>
