71f1c804d7d533302c9456ecf2e68e2afe2c3191
claytonjr <james.r.clayton@gmail.com>
2013-06-19 10:20:58 -0300

    Create LibSecurity.php

---
+++ LibSecurity.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * LibSecurity - A library of functions for working with security. 
+ * @author James Clayton <james.r.clayton@gmail.com>
+ * @version 0.0.1
+ * @copyright (c) 2013, James Clayton
+ * @package LibSecurity
+ * @license http://opensource.org/licenses/ISC ISC License (ISC)
+ */
+
+?>
