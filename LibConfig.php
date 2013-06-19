1586f830b9662b5eadbcdba6822d5f995818e558
claytonjr <james.r.clayton@gmail.com>
2013-06-19 10:16:27 -0300

    Create LibConfig.php

---
+++ LibConfig.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * LibConfig - Config file for LibraryPHP 
+ * @author James Clayton <james.r.clayton@gmail.com>
+ * @version 0.0.1
+ * @copyright (c) 2013, James Clayton
+ * @package LibConfig
+ * @license http://opensource.org/licenses/ISC ISC License (ISC)
+ */
+
+include_once('LibDatabase.php');
+include_once('LibDataManipulation.php');
+include_once('LibDate.php');
+include_once('LibFileSystem.php');
+include_once('LibFinancial.php');
+include_once('LibMath.php');
+include_once('LibNetwork.php');
+include_once('LibScience.php');
+include_once('LibSecurity.php');
+include_once('LibString.php');
+include_once('LibUtility.php');
+
+$LibVersion = '0.0.1';
+
+?>
