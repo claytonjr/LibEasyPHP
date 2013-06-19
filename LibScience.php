328e4ac3ae3a30c1699dacef2417441ea2dc43b4
claytonjr <james.r.clayton@gmail.com>
2013-06-19 09:20:35 -0400

    Create LibScience.php

---
+++ LibScience.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * LibScience - A library of functions for working with science. 
+ * @author James Clayton <james.r.clayton@gmail.com>
+ * @version 0.0.1
+ * @copyright (c) 2013, James Clayton
+ * @package LibScience
+ * @license http://opensource.org/licenses/ISC ISC License (ISC)
+ */
+
+?>
