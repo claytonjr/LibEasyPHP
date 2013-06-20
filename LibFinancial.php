ec73dbd76f364c4d793d7e810df58cc3914b545d
James Clayton <james.r.clayton@gmail.com>
2013-06-19 23:13:40 -0300

    Update LibFinancial.php

--- LibFinancial.php
+++ LibFinancial.php
@@ -10,7 +10,7 @@
  */
 
 class Financial {
-  
+	
 	/**
      * Mortgage() returns a number, how much a mortgage will cost. 
      * @param number $Apr Required. No default. The annual percentage rate of the loan. 
@@ -31,20 +31,52 @@ class Financial {
 		return $RetVal;
 	}
 
-	public function FutureValue($Rate, $Periods, $PresentValue) {
+	/**
+	 * FutureValue() returns the future value of an investment based on periodic, constant payments and a constant interest rate. Alias for Fv(). 
+	 * @param number $Rate Required. Rate is the interest rate per period. For example, if you obtain an automobile loan at a 10 percent annual interest rate and make monthly payments, your interest rate per month is 10%/12, or 0.83%. You would enter 10%/12, or 0.83%, or 0.0083, into the formula as the rate. 
+	 * @param number $Periods Required. Periods is the total number of payment periods in an annuity. For example, if you get a four-year car loan and make monthly payments, your loan has 4*12 (or 48) periods. You would enter 48 into the formula for periods. 
+	 * @param number $PresentValue Optional. Default is 0. Pv is the present value, or the lump-sum amount that a series of future payments is worth right now. If pv is omitted, it is assumed to be 0 (zero), and you must include the pmt argument.
+	 * @return number $FutureValue Returns the future value of an investment based on periodic, constant payments and a constant interest rate.
+	 */
+
+	public function FutureValue($Rate, $Periods, $PresentValue = 0) {
 		$FutureValue = $PresentValue * (pow((1 + $Rate), $Periods));
 		return $FutureValue;
 	}
+
+	/**
+	 * Fv() returns the future value of an investment based on periodic, constant payments and a constant interest rate. Alias for FutureValue(). 
+	 * @param number $Rate Required. Rate is the interest rate per period. For example, if you obtain an automobile loan at a 10 percent annual interest rate and make monthly payments, your interest rate per month is 10%/12, or 0.83%. You would enter 10%/12, or 0.83%, or 0.0083, into the formula as the rate. 
+	 * @param number $Periods Required. Periods is the total number of payment periods in an annuity. For example, if you get a four-year car loan and make monthly payments, your loan has 4*12 (or 48) periods. You would enter 48 into the formula for periods. 
+	 * @param number $PresentValue Optional. Default is 0. Pv is the present value, or the lump-sum amount that a series of future payments is worth right now. If pv is omitted, it is assumed to be 0 (zero), and you must include the pmt argument.
+	 * @return number $FutureValue Returns the future value of an investment based on periodic, constant payments and a constant interest rate.
+	 */
 
 	public function Fv($Rate, $Periods, $PresentValue) {
 		$FutureValue = $PresentValue * (pow((1 + $Rate), $Periods));
 		return $FutureValue;
 	}
 
+	/**
+	 * PresentValue() returns the present value of an investment. The present value is the total amount that a series of future payments is worth now. For example, when you borrow money, the loan amount is the present value to the lender. Alias for Pv(). 
+	 * @param number $Rate Required. Rate is the interest rate per period. For example, if you obtain an automobile loan at a 10 percent annual interest rate and make monthly payments, your interest rate per month is 10%/12, or 0.83%. You would enter 10%/12, or 0.83%, or 0.0083, into the formula as the rate. 
+	 * @param number $Periods Required. Periods is the total number of payment periods in an annuity. For example, if you get a four-year car loan and make monthly payments, your loan has 4*12 (or 48) periods. You would enter 48 into the formula for periods. 
+	 * @param number $FutureValue Required. Future Value is the future value, or a cash balance you want to attain after the last payment is made. If fv is omitted, it is assumed to be 0 (the future value of a loan, for example, is 0). For example, if you want to save $50,000 to pay for a special project in 18 years, then $50,000 is the future value. You could then make a conservative guess at an interest rate and determine how much you must save each month. If fv is omitted, you must include the pmt argument.
+	 * @return number $PresentValue Returns the present value of an investment. The present value is the total amount that a series of future payments is worth now. For example, when you borrow money, the loan amount is the present value to the lender.
+	 */
+
 	public function PresentValue($Rate, $Periods, $FutureValue) {
 		$PresentValue = $FutureValue / (pow((1 + $Rate), $Periods));
 		return $PresentValue;
 	}
+
+	/**
+	 * Pv() Returns the present value of an investment. The present value is the total amount that a series of future payments is worth now. For example, when you borrow money, the loan amount is the present value to the lender. Alias for PresentValue(). 
+	 * @param number $Rate Required. Rate is the interest rate per period. For example, if you obtain an automobile loan at a 10 percent annual interest rate and make monthly payments, your interest rate per month is 10%/12, or 0.83%. You would enter 10%/12, or 0.83%, or 0.0083, into the formula as the rate. 
+	 * @param number $Periods Required. Periods is the total number of payment periods in an annuity. For example, if you get a four-year car loan and make monthly payments, your loan has 4*12 (or 48) periods. You would enter 48 into the formula for periods. 
+	 * @param number $FutureValue Required. Future Value is the future value, or a cash balance you want to attain after the last payment is made. If fv is omitted, it is assumed to be 0 (the future value of a loan, for example, is 0). For example, if you want to save $50,000 to pay for a special project in 18 years, then $50,000 is the future value. You could then make a conservative guess at an interest rate and determine how much you must save each month. If fv is omitted, you must include the pmt argument.
+	 * @return number $PresentValue Returns the present value of an investment. The present value is the total amount that a series of future payments is worth now. For example, when you borrow money, the loan amount is the present value to the lender.
+	 */
 
 	public function Pv($Rate, $Periods, $FutureValue) {
 		$PresentValue = $FutureValue / (pow((1 + $Rate), $Periods));
